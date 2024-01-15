f43b5bcbf6461146f3758d5697da77d7b77dfa68
Onlysockets <150627591+Onlysockets@users.noreply.github.com>
2024-01-15 17:53:45 +0100

    Update VpnMenuConsole.sh
    ## Actualización  v1.2
    
    En la versión 1.2 se han añadido nuevas funcionalidades y mejoras significativas en el script:
    
    **Nuevas Funcionalidades:**
    1. **Hide (Ocultar VPN):**
       - Ahora puedes ocultar la VPN para aumentar la privacidad y la seguridad.
    
    2. **Unhide (Mostrar VPN Oculta):**
       - Se ha añadido la capacidad de volver a mostrar la VPN que se había ocultado.
    
    3. **Eliminar VPN:**
       - Opción para eliminar completamente la VPN según tus necesidades.
    
    **Mejoras de Rendimiento y Visualización:**
    - Se ha optimizado el rendimiento general del script para mayor eficiencia.
    - Mejoras visuales en la presentación del script para una experiencia de usuario más agradable.
    
    **Resolución de Errores:**
    - Se han corregido varios errores, mejorando la estabilidad y confiabilidad del script.

--- VpnMenuConsole.sh
+++ VpnMenuConsole.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
+#He actualizado el script a fecha de 15/01/2024, el manual.pdf esta desactualizado, sin embargo la parte importante(instalacion) no se ve afectada.
+#Es posible que necesites instalar Lolcat --> sudo apt install lolcat
 
+#Variables Globales
 
 # Ruta a la carpeta donde se encuentran los archivos .ovpn , edita esto a tu gusto.
-RUTA_VPNS="/home/usuario/Documents/vpn/"
+RUTA_VPNS="/home/username/Documents/vpn/"
 
+#Carpeta oculta, edita esto a tu gusto
+carpeta_oculta="$RUTA_VPNS/hidden_vpn/"
 
+#version del script
+version='1.2'
 
 #FUNCION CONTROL C
 
@@ -36,6 +43,129 @@ for ((i = 0; i < ${#archivos_ovpn[@]}; i++)); do
 done
 exit 0
 }
+
+ function ocultar_vpn {
+    id_a_ocultar=$2
+    archivos_ovpn=($RUTA_VPNS*.ovpn)
+    carpeta_oculta="$RUTA_VPNS/hidden_vpn"
+
+    if [ ! -d "$carpeta_oculta" ]; then
+        echo -e "Se ha creado la carpeta oculta, debido a que no existia previamente"
+        mkdir "$carpeta_oculta"
+    fi
+
+    if [ ${#archivos_ovpn[@]} -eq 0 ]; then
+        echo -e "\e[91m[!] No se encontraron archivos .ovpn en la ruta especificada.\e[0m"
+        exit 1
+    fi
+
+    if (( id_a_ocultar < 1 || id_a_ocultar > ${#archivos_ovpn[@]} )); then
+        echo -e "\e[91m[!] Identificador no válido.\e[0m"
+        exit 1
+    fi
+
+    archivo_a_ocultar="${archivos_ovpn[$id_a_ocultar - 1]}"
+    nombre_archivo=$(basename "$archivo_a_ocultar")
+
+    # Crea la carpeta oculta si no existe
+
+
+    # Mueve el archivo a la carpeta oculta
+    mv "$archivo_a_ocultar" "$carpeta_oculta/$nombre_archivo"
+    echo -e "\e[92m[+] VPN ocultada: \e[0m\e[33m$nombre_archivo\e[0m\e[93m\n\e[0m"
+    exit 0
+}
+
+
+function mostrar_vpn() {
+    carpeta_oculta="$RUTA_VPNS/hidden_vpn"
+
+    if [ ! -d "$carpeta_oculta" ]; then
+        echo -e "\e[91m[!] No se encontró la carpeta oculta.\e[0m"
+        exit 1
+    fi
+
+    archivos_ocultos=$(find "$carpeta_oculta" -maxdepth 1 -type f -name "*.ovpn")
+
+    if [ -z "$archivos_ocultos" ]; then
+        echo -e "\e[93mNo existen VPNs ocultas.\e[0m"
+        exit 0
+    else
+        echo "-----------------------------------------"
+        echo -e "\e[92m           VPNs ocultadas\e[0m"
+        echo "-----------------------------------------"
+        
+        contador=1
+        for archivo in $archivos_ocultos; do
+            nombre_archivo=$(basename "$archivo")
+            echo -e "\e[96mID: $contador   Archivo: $nombre_archivo\e[0m"
+            ((contador++))
+        done
+
+        echo -en "\e[96m¿Qué VPN quieres desocultar? Indica su ID --> \e[0m"
+        read id_a_desocultar
+
+        if (( id_a_desocultar < 1 || id_a_desocultar > $(echo "$archivos_ocultos" | wc -l) )); then
+            echo -e "\e[91m[!] ID Inválido! Solo existen estas VPNS ocultas:\e[0m"
+            contador=1
+            for archivo in $archivos_ocultos; do
+                nombre_archivo=$(basename "$archivo")
+                echo -e "\e[96mID: $contador   Archivo: $nombre_archivo\e[0m"
+                ((contador++))
+            done
+            exit 1
+        fi
+
+        archivo_a_desocultar=$(echo "$archivos_ocultos" | sed -n "${id_a_desocultar}p")
+        nombre_archivo=$(basename "$archivo_a_desocultar")
+
+        echo -e "\e[92m[+] VPN desocultada: $nombre_archivo\e[0m"
+        mv "$archivo_a_desocultar" "$RUTA_VPNS/$nombre_archivo"
+        exit 0
+    fi
+}
+
+
+
+
+function eliminar_vpn {
+    id_a_eliminar=$2
+    archivos_ovpn=($RUTA_VPNS*.ovpn)
+
+    if [ ${#archivos_ovpn[@]} -eq 0 ]; then
+        echo -e "\e[91m[!] No se encontraron archivos .ovpn en la ruta especificada.\e[0m"
+        exit 1
+    fi
+
+    if (( id_a_eliminar < 1 || id_a_eliminar > ${#archivos_ovpn[@]} )); then
+        echo -e "\e[91m[!] Identificador no válido.\e[0m Debes indicar el VPN_ID"
+        exit 1
+    fi
+
+    archivo_a_eliminar="${archivos_ovpn[$id_a_eliminar - 1]}"
+    confirmacion=""
+    
+    while [[ "$confirmacion" != "s" && "$confirmacion" != "n" ]]; do
+        #echo -e "\e[93m[?] ¿Estás seguro que quieres eliminar la VPN: $(basename "$archivo_a_eliminar")? (s/n)\e[0m"
+        echo -e "\nVPN SELECCIONADA:  \e[93m\e[0m\e[33m$(basename "$archivo_a_eliminar")\e[0m\e[93m\e[0m\n"
+        read -p "¿Estás seguro que quieres eliminar la VPN (s/n): " confirmacion
+        
+        if [[ "$confirmacion" != "s" && "$confirmacion" != "n" ]]; then
+            echo -e "\033[A\033[K\e[93m[!] Responde solo 's' o 'n': \e[0m"
+        fi
+    done
+
+    if [ "$confirmacion" = "s" ]; then
+        rm "$archivo_a_eliminar"
+        echo -e "\e[92m[+] VPN eliminada: $(basename "$archivo_a_eliminar")\e[0m"
+    else
+        echo -e "\e[93m[-] Operación cancelada.\e[0m"
+    fi
+    exit 0
+}
+
+
+
 
 function verificar_vpn_en_uso {
     if pgrep openvpn > /dev/null; then
@@ -93,18 +223,31 @@ function iniciar_vpn {
     fi
 }
 
+
 function mostrar_ayuda {
-    echo -e "\n\e[96m[?] Uso: <command> <argument>\n"
-    echo -e "\e[96m\t -s/--start: Iniciar VPN\e[0m (Debes incluir el VPN_ID)" 
+    echo -e "\n\e[96m[?] Uso: <comando> <argumento>\n"
+    echo -e "\e[96m\t -s/--start: Iniciar VPN\e[0m <VPN_ID>" 
     echo -e "\e[96m\t -st/--stop: Detener VPN\e[0m"
-    echo -e "\e[96m\t -l/--list: VPNs Disponibles + ID\e[0m"
-    echo -e "\e[96m\t -c/--creator: Muestra el creador de la herramienta\e[0m"
+    echo -e "\e[96m\t -l/--list: Mostrar VPNs Disponibles\e[0m <VPN_ID> "
+    echo -e "\e[96m\t -c/--creator: Mostrar el creador de la herramienta\e[0m"
+    echo -e "\e[96m\t -r/--remove: eliminar_vpn\e[0m <VPN_ID>"
+    echo -e "\e[96m\t -hi/--hide: ocultar_vpn\e[0m <VPN_ID>"
+    echo -e "\e[96m\t -uh/--unhide Hacer Visible una Vpn oculta\e[0m <VPN_ID>"
+    echo -e "\e[96m\t -v/--version Mostrar version del script \e[0m"
 }
 
 
 function show_creator(){
 echo -e "Created By Julangas Alias \"Onlysockets\"" | lolcat -s 5 
 }
+
+function version_checker(){
+    tput civis
+    sleep 0.3
+    echo -e "Version ---> $version" | pv -qL 20
+    tput cnorm
+}
+
 
 # Comprueba si se proporciona algún argumento
 if [ $# -eq 0 ]; then
@@ -113,9 +256,19 @@ if [ $# -eq 0 ]; then
 fi
 
 case "$1" in
- "--start" | "-s") iniciar_vpn "$@" ;;
+    "--start" | "-s") iniciar_vpn "$@" ;;
     "--stop" | "-st") detener_vpn ;;
     "--list" | "-l") listar_vpns ;;
     "--creator" | "-c") show_creator ;;
-    *) echo -e "\n\e[91m[!] Argumento no válido.\e[0m \n"; mostrar_ayuda; exit 1 ;;
+    "--remove" | "-r") eliminar_vpn "$@" ;;
+    "--version" | "-v") version_checker ;;
+    "--hide" | "-hi") ocultar_vpn "$@" ;;
+    "--unhide" | "-uh")
+        if [ -n "$2" ]; then
+            echo -e "\n\e[91m[!] Error de sintaxis.\e[0m Uso correcto: -uh/--unhide"
+            exit 1
+        else
+            mostrar_vpn "$@"
+        fi ;;
+    *) mostrar_ayuda ;;
 esac
